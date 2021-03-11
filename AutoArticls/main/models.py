699e850fc54f69cb3fd3134fc02b3b5a9d9ffab8
nik.saenko2002@mail.ru <nik.saenko2002@mail.ru>
2021-03-11 14:34:11 +0700

    all

--- AutoArticls/main/models.py
+++ AutoArticls/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+
+	article_title = models.CharField( 'Название статьи', max_length = 200)
+	article_text = models.TextField('Текст статьи')
+	release_date = models.DateField('Дата публикации')
+
+	class Meta:
+		verbose_name = "Статья"
+		verbose_name_plural = 'Статьи'
+
+class Comment(models.Model):
+
+	Article = models.ForeignKey(Article, on_delete=models.CASCADE)
+	name = models.CharField('Авто комментария' , max_length = 50)
+	comment_text = models.TextField('Текст комментария')
+	release_date_comment = models.DateField('Дата публикации')
+
+	class Meta:
+		verbose_name = "Комментарий"
+		verbose_name_plural = 'Комментарии'
